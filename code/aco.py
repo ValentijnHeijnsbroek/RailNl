62a7d56052174fffa08034a31a893ad7de611be2
borisremmers <boris.remmers@gmail.com>
2024-01-25 13:35:21 +0100

    aco.py now fully functional except for max duration constraint

--- code/aco.py
+++ code/aco.py
@@ -39,7 +39,7 @@ class ACO():
         self.ants = []
         self.current_station = None
         self.totaal_trajecten = {}
-
+        self.unique_connections = set()
         
         
     def set_pheromones(self, connection_filename):
@@ -50,7 +50,7 @@ class ACO():
                 station_1 = row[0]
                 station_2 = row[1]
                 self.pheromones[(station_1, station_2)] = 1
-        print("pheromones set", self.pheromones)
+        # print("pheromones set", self.pheromones)
         
     def deploy_ants(self, rail_network, num_ants):
         for i in range(num_ants):
@@ -83,17 +83,17 @@ class ACO():
             
             # Calculate the probabilities for each possible connection based on pheromone levels
             probabilities = [find_pheromones[connection.name] / total_pheromone for connection in possible_connections]
-            print("Probabilities:", probabilities)
-            # Choose the next connection based on the probabilities
-            print("Possible connections:", possible_connections)
+            # print("Probabilities:", probabilities)
+            # # Choose the next connection based on the probabilities
+            # print("Possible connections:", possible_connections)
             next_connection = choices(possible_connections, weights = probabilities)[0]
-            print("Next connection:", next_connection)
+            # print("Next connection:", next_connection)
             
             return next_connection
         
             
         else:
-            print("No connections")
+            # print("No connections")
             return None
         
     def update_pheromones(self, rail_network, evaporation_rate):
@@ -108,7 +108,7 @@ class ACO():
             self.pheromones[connection] *= (1 - evaporation_rate)
         
         # Update pheromones on connections in the best solution
-        print("Best solution:", best_solution)  
+        # print("Best solution:", best_solution)  
         for connection in best_solution:
             for i in range(len(best_solution) - 1):
                 station_1 = best_solution[i]
@@ -118,20 +118,44 @@ class ACO():
                         self.pheromones[(station_1.name, station_2.name)] += 1 / best_score
                     except KeyError:
                         self.pheromones[(station_2.name, station_1.name)] += 1 / best_score
-                    print("pheromones:", self.pheromones)
+                    # print("pheromones:", self.pheromones)
                     
+    def total_score(self):
+        total_score = 0
+        duration_traject = 0
+        duration_totaal = 0
+        for ant in self.ants:
+            for i in range(len(ant.traject) - 1):  # Loop until the second to last element
+                station_1 = ant.traject[i]
+                station_2 = ant.traject[i+1]
+                connection = (station_1.name, station_2.name)
+                self.update_unique_connections(connection) 
+                if station_1.connections_durations[station_2]:
+                    duration_traject += station_1.connections_durations[station_2]      
+            duration_totaal += duration_traject
+        # print("Unique connections:", self.unique_connections)   
+        print("length unique connections:", len(self.unique_connections))      
+        p = len(self.unique_connections) / rail_network.amount_of_connections
+        print("p:", p)
+        k = p * 10000 - (len(self.ants) * 100 + duration_totaal)
+        print("duration_totaal:", duration_totaal)  
+        return round(k, 2)
+        
+    
+    def update_unique_connections(self, connection):
+        self.unique_connections.add(tuple(sorted(connection)))
 # Initialize parameters
-num_ants = 10
-num_iterations = 10
-evaporation_rate = 0.1
+num_ants = 20
+num_iterations = 1000
+evaporation_rate = 0.05
 
 #main loop
 if __name__ == "__main__":
     rail_network = RailNL()
     aco = ACO()
-    rail_network.load_stations('StationsHolland.csv')
-    rail_network.load_connections('ConnectiesHolland.csv')
-    aco.set_pheromones('ConnectiesHolland.csv')
+    rail_network.load_stations('StationsNationaal.csv')
+    rail_network.load_connections('ConnectiesNationaal.csv')
+    aco.set_pheromones('ConnectiesNationaal.csv')
 
     for i in range(num_iterations):
         ants = aco.deploy_ants(rail_network, num_ants)
@@ -141,24 +165,24 @@ if __name__ == "__main__":
                 ant.current_station = aco.choose_next_station(ant, aco.pheromones, 7)
                 if ant.current_station is not None:
                     ant.traject.append(ant.current_station)
+                    aco.update_unique_connections((ant.traject[-2].name, ant.traject[-1].name))
             
             aco.update_pheromones(rail_network, evaporation_rate)
-            
+            aco.totaal_trajecten[ant] = ant.traject
         # Print information after each iteration
         print("Iteration:", i + 1)
-        for ant in ants:
-            print("Score:", ant.get_score(rail_network))
-            print("Trajectory:", ant.traject)
-            print("Pheromones:", aco.pheromones)
-            aco.totaal_trajecten[ant] = ant.traject
-        print("Totaal trajecten:", aco.totaal_trajecten)
-        score_totaal = rail_network.get_score()
+        # for ant in ants:
+        #     print("Score:", ant.get_score(rail_network))
+        #     print("Trajectory:", ant.traject)
+        #     print("Pheromones:", aco.pheromones)
+        
+        # print("Totaal trajecten:")
+        for ant, trajectory in aco.totaal_trajecten.items():
+            station_names = [station.name for station in trajectory]
+            print("Trajectory:", station_names)
+
+        
+        # print("Totaal trajecten:", aco.totaal_trajecten)
+        score_totaal = aco.total_score()
         print("Score totaal:", score_totaal)    
         print("------")
-        
-
-
-
-
-
-
