4d8c442a1b62adfae44ea67ef79c8baed87023a6
Mohammed Zouhri <103220677+RealMasSafe@users.noreply.github.com>
2024-01-26 10:23:56 +0100

    Update depth_first.py

--- code/depth_first.py
+++ code/depth_first.py
@@ -1,16 +1,18 @@
 import random
 import copy
 import pandas as pd
-from help_funtions import initialize_rail, greedy_decision
+from help_funtions import *
+import matplotlib.pyplot as plt 
 
 # Load the connection data
-file_path = '../data/ConnectiesHolland.csv'
+file_path = '../data/ConnectiesNationaal.csv'
 connection_data = pd.read_csv(file_path)
 
 # Constants
-max_aantal_minuten = 160
+max_aantal_minuten = 180
 max_aantal_trajecten = 20
-threshold_visit_frequency = 3  # Adjust as needed
+threshold_visit_frequency = 6  # Adjust as needed
+MAX_ATTEMPTS = 560
 
 # Function to find central hubs
 def find_central_hubs(connection_data):
@@ -21,57 +23,106 @@ def find_central_hubs(connection_data):
 central_hubs = find_central_hubs(connection_data)
 
 def can_add_station(traject, station, visited_stations, station_visit_frequency):
-    if len(traject.traject_stations) >= 10:
+    if len(traject.traject_stations) > 0:
+        last_station = traject.traject_stations[-1]
+        if not ((connection_data['station1'] == last_station.name) & (connection_data['station2'] == station.name)).any() and \
+           not ((connection_data['station2'] == last_station.name) & (connection_data['station1'] == station.name)).any():
+            return False
+    if len(traject.traject_stations) >= 20:
         return False
     if station_visit_frequency.get(station.name, 0) > threshold_visit_frequency:
         return False
     return True
 
-def depth_first_search(rail, traject, traject_index, visited_stations, uncovered_stations, current_depth, max_depth, current_score, central_hubs):
-    station_visit_frequency = {}
+def update_uncovered_connections(uncovered_connections, traject):
+    if len(traject.traject_stations) >= 2:
+        # Get the last two stations in the traject
+        last_station = traject.traject_stations[-2]
+        new_station = traject.traject_stations[-1]
 
+        # Create connection tuple
+        connection = (last_station.name, new_station.name)
+
+        # Remove the connection from uncovered connections
+        if connection in uncovered_connections:
+            uncovered_connections.remove(connection)
+        # Check and remove the reverse connection as well
+        reverse_connection = (new_station.name, last_station.name)
+        if reverse_connection in uncovered_connections:
+            uncovered_connections.remove(reverse_connection)
+
+
+def depth_first_search(rail, traject, traject_index, visited_stations, uncovered_connections, current_depth, max_depth, current_score, central_hubs):
     if current_depth >= max_depth:
         return False
-
+    station_visit_frequency = {}
     score_improved = False
+    best_new_score = current_score
+    best_new_station = None
 
-    next_station = greedy_decision(rail, traject_index)
+    # Explore potential next stations
+    for next_station in rail.stations:
+        if next_station not in visited_stations and can_add_station(traject, next_station, visited_stations, station_visit_frequency):
+            traject.add_station_to_traject(next_station)
+            visited_stations.add(next_station)
+            update_uncovered_connections(uncovered_connections, traject)
 
-    if next_station and can_add_station(traject, next_station, visited_stations, station_visit_frequency):
-        traject.add_station_to_traject(next_station)
-        visited_stations.add(next_station)
-        station_visit_frequency[next_station.name] = station_visit_frequency.get(next_station.name, 0) + 1
-
-        if rail.sum_time(traject_index) <= max_aantal_minuten:
             new_score = rail.get_score()
-            # Ensure traject has more than one station before updating the score
-            if (new_score > current_score or len(traject.traject_stations) < 2) and len(traject.traject_stations) > 1:
-                score_improved = True
-                if depth_first_search(rail, traject, traject_index, visited_stations, uncovered_stations, current_depth + 1, max_depth, new_score, central_hubs):
+
+            # Check for score improvement and adherence to time constraints
+            if new_score > best_new_score and rail.sum_time(traject_index) <= max_aantal_minuten:
+                best_new_score = new_score
+                best_new_station = next_station
+
+                # Recursive search with updated score and depth
+                if depth_first_search(rail, traject, traject_index, visited_stations, uncovered_connections, current_depth + 1, max_depth, new_score, central_hubs):
                     return True
 
-        visited_stations.remove(next_station)
-        traject.delete_latest_station()
+            # Revert changes for the next iteration
+            visited_stations.remove(next_station)
+            traject.delete_latest_station()
+    if len(traject.traject_stations) < 3:
+        for i in range(len(traject.traject_stations)):
+            traject.delete_latest_station()
+        return False
+    # Check if a better station was found
+    if best_new_station:
+        traject.add_station_to_traject(best_new_station)
+        visited_stations.add(best_new_station)
+        update_uncovered_connections(uncovered_connections, traject)
+        score_improved = True
 
     return score_improved
 
-
-def iterative_depth_first(max_depth, iterations, central_hubs):
+iteration_scores = []
+def iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_threshold=1000):
     best_rail = None
     best_score = 0
+    iterations_without_improvement = 0
+    uncovered_connections = set()
+    attempts = 0
+
+    for _, row in connection_data.iterrows():
+        uncovered_connections.add((row['station1'], row['station2']))
+        uncovered_connections.add((row['station2'], row['station1']))
 
     for iteration in range(iterations):
+        if iterations_without_improvement >= no_improvement_threshold:
+            central_hubs = find_central_hubs(connection_data)
+            iterations_without_improvement = 0
+        
         print(f"Progress: {iteration / iterations * 100:.2f}%")
-        current_rail = initialize_rail('Holland')
+        current_rail = initialize_rail()
         valid_traject_count = 0
         new_score = 0
+        traject_index = 0
 
-        for traject_index in range(1, max_aantal_trajecten + 1):
+        while valid_traject_count < max_aantal_trajecten:
+            traject_index += 1
             new_traject = current_rail.create_traject(traject_index)
             
-            # Prefer central hubs as start stations
             if central_hubs:
-                start_station_name = central_hubs.pop(0)
+                start_station_name = central_hubs.pop()
                 start_station = next((s for s in current_rail.stations if s.name == start_station_name), None)
             else:
                 start_station = random.choice(current_rail.stations)
@@ -79,27 +130,52 @@ def iterative_depth_first(max_depth, iterations, central_hubs):
             new_traject.add_station_to_traject(start_station)
             visited_stations = set([start_station])
 
-            traject_improved = depth_first_search(current_rail, new_traject, traject_index, visited_stations, set(current_rail.stations), 0, max_depth, 0, central_hubs)
-
-            if len(new_traject.traject_stations) > 1:
+            traject_improved = depth_first_search(current_rail, new_traject, traject_index, visited_stations, uncovered_connections, 0, max_depth, 0, central_hubs)
+            
+            if len(new_traject.traject_stations) >= 4:
                 valid_traject_count += 1
                 new_score = current_rail.get_score()
+        
                 if new_score > best_score:
                     best_score = new_score
                     best_rail = copy.deepcopy(current_rail)
-
-            if valid_traject_count >= max_aantal_trajecten or new_score == best_score:
+                    iterations_without_improvement = 0
+                else:
+                    iterations_without_improvement += 1
+            else:
+                # If the trajectory is not valid, reduce the traject_index
+                traject_index -= 1
+            if attempts > MAX_ATTEMPTS:
+                iteration_scores.append(new_score) 
+                attempts = 0
                 break
+            if new_score == best_score:
+                iteration_scores.append(new_score) 
+                attempts += 1
+            else:
+                attempts += 1
 
         print(f"End of iteration {iteration + 1}, Best score: {best_score}")
 
     if best_rail:
         best_rail.print_output()
+        best_rail.upload_output('depth_first_search.csv')
     else:
         print("No optimal rail configuration found.")
+
+    plt.hist(iteration_scores, bins=50, color='blue', alpha=0.7)
+    plt.xlabel('Scores')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Scores')
+    plt.grid(True)
+    plt.savefig('../data/depthfirst_histogram.png')
+
+    best_rail.plot_network()
+
     return best_rail
 
 # Example usage
-max_depth = 20
-iterations = 50000
-best_rail = iterative_depth_first(max_depth, iterations, central_hubs)
+max_depth = 60
+iterations = 100
+no_improvement_threshold = 500
+best_rail = iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_threshold)
