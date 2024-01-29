275a07771b46a7844ee04f826e54734054b1408a
Mas <mohammed.zouhri.12@gmail.com>
2024-01-29 15:02:42 +0100

    documentation in station, traject depth_first

--- code/depth_first.py
+++ code/depth_first.py
@@ -1,52 +1,54 @@
 import random
 import copy
 import pandas as pd
-from help_funtions import *
-import matplotlib.pyplot as plt 
+from help_funtions import initialize_rail
+from railnl import RailNL
+import matplotlib.pyplot as plt
+from station import Station
+from traject import Traject
+from typing import List, Set
 
 # Load the connection data
-file_path = '../data/ConnectiesNationaal.csv'
-connection_data = pd.read_csv(file_path)
+file_path: str = '../data/ConnectiesNationaal.csv'
+connection_data: pd.DataFrame = pd.read_csv(file_path)
 
 # Constants
 max_aantal_minuten: int = 180
-max_aantal_trajecten: int = 20
+max_aantal_trajecten: int = 15
 threshold_visit_frequency: int = 9  # Adjust as needed
-MAX_ATTEMPTS: int = 50
-min_stations: int = 3
+MAX_ATTEMPTS: int = 300
+min_stations: int = 2
 
 # Function to find central hubs
-def find_central_hubs(connection_data):
+def find_central_hubs(connection_data: pd.DataFrame) -> List[str]:
     """
     Makes a list of the stations with the most connections to the least connection
 
-    pre: connection dat of each station
+    pre: connection data of each station
     post: central hubs list
     """
-    connection_counts = (connection_data['station1'].value_counts() + connection_data['station2'].value_counts()).fillna(0)
-    central_hubs = connection_counts.sort_values(ascending=False).index.tolist()
+    connection_counts: pd.Series = (connection_data['station1'].value_counts() + connection_data['station2'].value_counts()).fillna(0)
+    central_hubs: List[str] = connection_counts.sort_values(ascending=False).index.tolist()
     return central_hubs
 
 # make the list global
-central_hubs: list = find_central_hubs(connection_data)
+central_hubs: List[str] = find_central_hubs(connection_data)
 
-def can_add_station(traject, station, visited_stations, station_visit_frequency):
+def can_add_station(traject: RailNL, station: Station, visited_stations: Set[Station], station_visit_frequency: dict[Station: int]) -> bool:
     """
-    Checks whether the station can be added to the the traject or not
+    Checks whether the station can be added to the traject or not
 
-    pre: traject object, station object and a variable that checks how many time the station has
+    pre: traject object, station object and a variable that checks how many times the station has
         been in the rail
-    post: True or False 
-    
+    post: True or False
     """
-
     if len(traject.traject_stations) > 0:
-        last_station = traject.traject_stations[-1]
+        last_station: Station = traject.traject_stations[-1]
         if last_station.name == station.name:
             return False
-            
+
     if len(traject.traject_stations) > 0:
-        last_station = traject.traject_stations[-1]
+        last_station: Station = traject.traject_stations[-1]
         if not ((connection_data['station1'] == last_station.name) & (connection_data['station2'] == station.name)).any() and \
            not ((connection_data['station2'] == last_station.name) & (connection_data['station1'] == station.name)).any():
             return False
@@ -56,49 +58,50 @@ def can_add_station(traject, station, visited_stations, station_visit_frequency)
         return False
     return True
 
-def update_uncovered_connections(uncovered_connections, traject):
+def update_uncovered_connections(uncovered_connections: Set[tuple], traject: Traject) -> None:
     """
-    When a connection has been made it has been covered and then that connection has to 
-    be removed from the uncovered list 
+    When a connection has been made it has been covered and then that connection has to
+    be removed from the uncovered list
 
     pre: uncovered connection set, and traject object
     post: updated uncovered connection list for the rail
     """
     if len(traject.traject_stations) >= 2:
         # Get the last two stations in the traject
-        last_station = traject.traject_stations[-2]
-        new_station = traject.traject_stations[-1]
+        last_station: Station = traject.traject_stations[-2]
+        new_station: Station = traject.traject_stations[-1]
 
         # Create connection tuple
-        connection = (last_station.name, new_station.name)
+        connection: tuple = (last_station.name, new_station.name)
 
         # Remove the connection from uncovered connections
         if connection in uncovered_connections:
             uncovered_connections.remove(connection)
         # Check and remove the reverse connection as well
-        reverse_connection = (new_station.name, last_station.name)
+        reverse_connection: tuple = (new_station.name, last_station.name)
         if reverse_connection in uncovered_connections:
             uncovered_connections.remove(reverse_connection)
 
-
-def depth_first_search(rail, traject, traject_index, visited_stations, uncovered_connections, current_depth, max_depth, current_score, central_hubs):
+def depth_first_search(rail: RailNL, traject: Traject, traject_index: int, visited_stations: Set[Station],
+                      uncovered_connections: Set[tuple], current_depth: int, max_depth: int, current_score: float,
+                      central_hubs: List[str]) -> bool:
     """
     Performs a depth-first search to add stations to a rail trajectory.
-    This function recursively explores potential stations to add, 
+    This function recursively explores potential stations to add,
     aiming to improve the overall score of the rail network.
-    
-    pre: rail object, traject object, traject index, 
-         visited_stations ser, uncovered connection set, current depth int
+
+    pre: rail object, traject object, traject index,
+         visited_stations set, uncovered connection set, current depth int
          max depth int, current score float, and a central hub list.
     post: returns True if adding a station improves the score and meets constraints, otherwise False.
           updates the trajectory and uncovered connections as stations are added.
     """
     if current_depth >= max_depth:
         return False
-    station_visit_frequency = {}
-    score_improved = False
-    best_new_score = current_score
-    best_new_station = None
+    station_visit_frequency: dict = {}
+    score_improved: bool = False
+    best_new_score: float = current_score
+    best_new_station: Station = None
 
     # Explore potential next stations
     for next_station in rail.stations:
@@ -107,7 +110,7 @@ def depth_first_search(rail, traject, traject_index, visited_stations, uncovered
             visited_stations.add(next_station)
             update_uncovered_connections(uncovered_connections, traject)
 
-            new_score = rail.get_score()
+            new_score: float = rail.get_score()
 
             # Check for score improvement and adherence to time constraints
             if new_score > best_new_score and rail.sum_time(traject_index) <= max_aantal_minuten:
@@ -135,25 +138,25 @@ def depth_first_search(rail, traject, traject_index, visited_stations, uncovered
     return score_improved
 
 # score list for histogram
-iteration_scores = []
-def iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_threshold=1000):
+iteration_scores: List[float] = []
+
+def iterative_depth_first(max_depth: int, iterations: int, central_hubs: List[str], no_improvement_threshold: int = 1000) -> RailNL:
     """
-    Conducts an iterative depth-first search to optimize a network of rail trajectories. 
+    Conducts an iterative depth-first search to optimize a network of rail trajectories.
     The function iterates over multiple attempts to construct an optimal set of trajectories
-    aiming to maximize a scoring function given constraints. 
+    aiming to maximize a scoring function given constraints.
 
     pre: max depth int, iterations int, central hubs list, and a no improvement threshold
-
     post: returns the best rail network configuration found after all iterations
           when a better rail network has been found it changes the best rail into that one
           when there is no improvement for a given amount of iterations the central hub list refreshes
-          stops trying to make a better railgiven a start station after a certain amount of attempts
+          stops trying to make a better rail given a start station after a certain amount of attempts
     """
-    best_rail = None
-    best_score = 0
-    iterations_without_improvement = 0
-    uncovered_connections = set()
-    attempts = 0
+    best_rail: RailNL = None
+    best_score: float = 0
+    iterations_without_improvement: int = 0
+    uncovered_connections: Set[tuple] = set()
+    attempts: int = 0
 
     for _, row in connection_data.iterrows():
         uncovered_connections.add((row['station1'], row['station2']))
@@ -163,33 +166,33 @@ def iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_th
         if iterations_without_improvement >= no_improvement_threshold:
             central_hubs = find_central_hubs(connection_data)
             iterations_without_improvement = 0
-        
+
         print(f"Progress: {iteration / iterations * 100:.2f}%")
-        current_rail = initialize_rail()
-        valid_traject_count = 0
-        new_score = 0
-        traject_index = 0
+        current_rail: RailNL = initialize_rail()
+        valid_traject_count: int = 0
+        new_score: float = 0
+        traject_index: int = 0
 
         while valid_traject_count < max_aantal_trajecten:
             traject_index += 1
-            new_traject = current_rail.create_traject(traject_index)
-            
+            new_traject: Traject = current_rail.create_traject(traject_index)
+
             if central_hubs:
-                start_station_name = random.choice(central_hubs)
+                start_station_name: str = random.choice(central_hubs)
                 central_hubs.remove(start_station_name)
-                start_station = next((s for s in current_rail.stations if s.name == start_station_name), None)
+                start_station: Station = next((s for s in current_rail.stations if s.name == start_station_name), None)
             else:
-                start_station = random.choice(current_rail.stations)
+                start_station: Station = random.choice(current_rail.stations)
 
             new_traject.add_station_to_traject(start_station)
-            visited_stations = set([start_station])
+            visited_stations: Set[Station] = set([start_station])
 
-            traject_improved = depth_first_search(current_rail, new_traject, traject_index, visited_stations, uncovered_connections, 0, max_depth, 0, central_hubs)
-            
+            traject_improved: bool = depth_first_search(current_rail, new_traject, traject_index, visited_stations, uncovered_connections, 0, max_depth, 0, central_hubs)
+
             if len(new_traject.traject_stations) >= min_stations:
                 valid_traject_count += 1
                 new_score = current_rail.get_score()
-        
+
                 if new_score > best_score:
                     best_score = new_score
                     best_rail = copy.deepcopy(current_rail)
@@ -200,11 +203,11 @@ def iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_th
                 # If the trajectory is not valid, reduce the traject_index
                 traject_index -= 1
             if attempts > MAX_ATTEMPTS:
-                iteration_scores.append(new_score) 
+                iteration_scores.append(new_score)
                 attempts = 0
                 break
             if new_score == best_score:
-                iteration_scores.append(new_score) 
+                iteration_scores.append(new_score)
                 attempts += 1
             else:
                 attempts += 1
@@ -229,7 +232,7 @@ def iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_th
     return best_rail
 
 # Example usage
-max_depth: int= 75
-iterations: int = 100
-no_improvement_threshold: int = 150
+max_depth: int = 75
+iterations: int = 1050
+no_improvement_threshold: int = 63
 best_rail: RailNL = iterative_depth_first(max_depth, iterations, central_hubs, no_improvement_threshold)
--- code/traject.py
+++ code/traject.py
@@ -1,29 +1,29 @@
-import csv
 from station import Station
+from typing import Any
 import random
 
 class Traject:
     def __init__(self):
-        self.duration = 0
-        self.traject_stations = []
-
-        # last_station = self.traject_stations[-1]
-        # self.list_possible_stations = [station for station in last_station.connections if not self.is_bereden(station)]
+        """
+        Initializes an empty Traject object with zero duration and no traject stations.
+        """
+        self.duration: float = 0
+        self.traject_stations: list[Station] = []
 
     
-    def add_station_to_traject(self, station: Station):
+    def add_station_to_traject(self, station: Station) -> None:
         """
-        Add stations to the traject, if it was already in the traject list it goes to the next one that is connected to the 
-        given station.
+        Add stations to the traject. If it was already in the traject list, it goes to the next one that is connected to the given station.
+
+        pre: station object to add to the traject
+        post: if the traject is empty, station is added to the traject_stations list.
+              if it isnt empty a connected station is added to the list and if it isnt in 
+              the traject already
         """
-        
         if len(self.traject_stations) == 0:
             self.traject_stations.append(station)
         else:
-
             last_station = self.traject_stations[-1]
-
-
             if station in last_station.connections and station not in self.traject_stations:
                 self.traject_stations.append(station)
             else:
@@ -32,36 +32,65 @@ class Traject:
                 elif station not in last_station.connections:
                     print(f"ERROR: Station {station.name} not in connection with traject")
     
-    def is_bereden(self, station):
+    def is_bereden(self, station: Station) -> bool:
+        """
+        Checks if a given station is already present in the traject.
+
+        pre: station object
+        post: bool like that give true if the station is in the traject_stations list and 
+              false if it isnt there
+        """
         return station in self.traject_stations
     
-    def delete_station(self, station):
+    def delete_station(self, station: Station) -> None:
+        """
+        Deletes a specific station from the traject_stations list if it exists in the list.
+
+        pre: station object 
+        post: removes the station if it is in the traject_stations list
+        """
         if station in self.traject_stations:
             self.traject_stations.remove(station)
     
-    def delete_latest_station(self):
+    def delete_latest_station(self) -> None:
+        """
+        Deletes the latest station from the traject_stations list, 
+        if the traject_stations list is not empty
+
+        post: traject deletes the latest station added if the traject_stations list in not empty
+        """
         if self.traject_stations:
             self.traject_stations.pop()
-        else:
-            #print("Cannot delete from an empty list.")
-            pass
 
     # Returns a random connected station
-    def random_connected_station(self):
+    def random_connected_station(self) -> Any:
+        """
+        Returns a random connected station to the last station in the traject_stations list.
+
+        post: if the traject is empty, 'station' is added to the traject_stations list.
+              if the traject_stations list is not empty and there are connected stations to the last station, a random connected station is returned.    
+        """
         if self.traject_stations:
             last_station = self.traject_stations[-1]
             list_possible_stations = [station for station in last_station.connections if not self.is_bereden(station)]
             if list_possible_stations:
-                
                 random_station = random.choice(list_possible_stations)
                 return random_station
             else:
-                # print("No connections")
+                print("No connections")
                 return None
         else:
-            print("Cannot add random station to an empty traject.")
+            print("Cannot add a random station to an empty traject.")
             return None
 
-    def last_station(self):
-        return self.traject_stations[-1]    
-    
+    def last_station(self) -> Any:
+        """
+        Returns the last station in the traject_stations list.
+
+        post: returns lastest addition to the traject if the traject_stations list isnt empty
+              otherwise it returns None 
+        """
+        if self.traject_stations:
+            return self.traject_stations[-1]
+        else:
+            return None
